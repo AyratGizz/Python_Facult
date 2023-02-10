cc3323c22d578df09f9f22d4acff923640226ffa
AyratGizz <gizzatullin-airat@mail.ru>
2023-02-11 03:10:55 +0500

    dz_3

--- facult_3/DZ_3/Homosapiens.py
+++ facult_3/DZ_3/Homosapiens.py
@@ -1,35 +1,41 @@
 class Homosapiens():
     def __init__(self, name, age, gender, nation, ):
-        self.name = name  # имя человека
-        self.age = age  # возраст человека
-        self.gender = gender  # пол человека
-        self.nation = nation  # нация человека
+        self.name = name  # имя
+        self.age = age  # возраст
+        self.gender = gender  # пол
+        self.nation = nation  # нация
 
     def get_name(self):
+        """Метод выводит имя экземпляра класса"""
         print(f'{self.name}')
 
     def adult(self):
+        """Метод определяет, является ли экземпляр класса совершеннолетним"""
         if self.age >= 18:
             print("Человек совершеннолетний, ему 18 и более лет!")
         else:
             print("Человек ещё не совершеннолетний")
 
     def went_to_work(self):
+        """Метод выводит, что экземпляр класса пошел на работу"""
         print(f'{self.name} пошёл на работу зарабатывать деньги')
 
     def resting_at_home(self):
+        """Метод выводит, что экземпляр класса отдыхает дома"""
         print(f'{self.name} отдыхает дома')
 
     def changed_gender(self):
-        if self.gender == "Мужской":
-            self.gender = "сменил свой пол на женский"
-            print(f'{self.name} {self.gender}')
-        else:
-            self.gender = "- не менял свой пол"
-            print(f'{self.name} {self.gender}')
+        """Метод класса который выводит пол экземпляра класса"""
+        print(f'{self.name} - {self.gender}')
 
     def get_nation(self):
-        print(f'{self.name} - {self.nation}')
+        """Метод меняет национальность экземпляра класса"""
+        if self.nation == "Русский":
+            self.nation = "хочет сменить свою религию на Мусульманин"
+            print(f'{self.name} {self.nation}')
+        else:
+            self.nation = "- не хочет менять свою религию"
+            print(f'{self.name} {self.nation}')
 
 
 ivan = Homosapiens("Иван", 23, "Мужской", "Русский")
